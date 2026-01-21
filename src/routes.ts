257c8e15206b8e4bb16afab4bda2a7c54dee8e3d
EduardoEricDev <eduardo.dev2022@gmail.com>
2026-01-21 10:53:05 -0300

    feat: add global error handling middleware with express-async-errors

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,16 @@
 import { Router, Request, Response } from "express";
 
 const router = Router();
-router.get("/test", (request: Request, response: Response) => {
-	return response.json({ ok: true });
-});
+
+// Rota para testar as rotas estão funcionando
+// router.get("/test", (request: Request, response: Response) => {
+// 	return response.json({ ok: true });
+// });
+
+// Rota para testar o middleware de erro
+// router.get('/test-error', async (req, res) => {
+//   // Simulando um erro que aconteceria, por exemplo, se um produto não fosse encontrado
+//   throw new Error("Este é um erro de teste capturado pelo middleware!");
+// });
 
 export { router };
